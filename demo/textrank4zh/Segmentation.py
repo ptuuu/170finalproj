70b0eba7643ed77ffdcf31a599b862c97323e373
ptuuu <youl10@uci.edu>
2020-06-13 06:49:59 +0800

    Amended

--- demo/textrank4zh/Segmentation.py
+++ demo/textrank4zh/Segmentation.py
@@ -3,12 +3,8 @@ import jieba
 import codecs
 
 class WordSegmentation(object):
-    ''' 分词 '''
     
     def __init__(self, stop_words_file = None, user_defined_dict = None):
-        '''
-        stop_words_file: 保存停止词的文件路径，每行一个停止次
-        '''
         
         self.default_speech_tag_filter = ['an', 'i', 'j', 'l', 'n', 'nr', 'nrfg', 'ns', 'nt', 'nz',
                                           't', 'v', 'vd', 'vn', 'eng']
@@ -21,15 +17,6 @@ class WordSegmentation(object):
             jieba.load_userdict(user_defined_dict)
     
     def segment(self, text, lower = True, with_stop_words = True, speech_tag_filter = False):
-        '''
-        speech_tag_filter: 若为bool变量且为True，则使用默认的self.default_speech_tag_filter过滤，
-                           若为list类型，则使用speech_tag_filter过滤
-                           否则，不过滤
-        with_stop_words:  若为True，则利用停止词集合来过滤（去掉停止词），否则什么都不做
-        
-        lower = True：     是否将单词小写（针对英文）
-        '''
-        
         jieba_result = pseg.cut(text)
         
         if type(speech_tag_filter) == bool and speech_tag_filter == True:
@@ -55,12 +42,6 @@ class WordSegmentation(object):
         return res
         
     def segment_sentences(self, sentences, lower = True, with_stop_words = True, speech_tag_filter=False):
-        '''
-        将列表sequences中的每个句子转换为由单词构成的列表。
-        
-        sequences：列表，每个元素是一个句子（字符串类型）
-        '''
-        
         res = []
         for sentence in sentences:
             res.append(self.segment(text=sentence, 
@@ -70,12 +51,8 @@ class WordSegmentation(object):
         return res
         
 class SentenceSegmentation(object):
-    ''' 分句 '''
     
     def __init__(self, delimiters='?!;？！。；…\n'):
-        '''
-        delimiters: 用来拆分句子
-        '''
         self.delimiters = str(delimiters)
 
             
@@ -94,10 +71,6 @@ class SentenceSegmentation(object):
 class Segmentation(object):
     
     def __init__(self, stop_words_file = None, user_defined_dict = None, delimiters='?!;？！。；…\n'):
-        '''
-        stop_words_file: 停止词文件
-        delimiters: 用来拆分句子
-        '''
         self.ws = WordSegmentation(stop_words_file, user_defined_dict)
         self.ss = SentenceSegmentation(delimiters)
         
